e2dbd50b46ddd01b5abf2029263862a4794834b3
Paul Bonser <misterpib@gmail.com>
2017-06-23 13:54:58 -0500

    Limit to aws provider, add IAM Role policy bits

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ class XrayEnablerPlugin {
     this.serverless = serverless;
     this.options = options;
 
+    this.provider = this.serverless.getProvider('aws');
+
     this.hooks = {
       'after:package:compileFunctions': this.beforeDeploy.bind(this),
     };
@@ -12,9 +14,20 @@ class XrayEnablerPlugin {
 
   beforeDeploy() {
     var resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
+
+    // Allow lambdas to write X-Ray traces
+    resources.IamRoleLambdaExecution.Properties.Policies[0].PolicyDocument.Statement.push({
+      Effect: 'Allow',
+      Action: [
+        'xray:PutTraceSegments',
+        'xray:PutTelemetryRecords',
+      ],
+      Resource: ['*'],
+    });
+
+    // Enable X-Ray on each lambda
     Object.keys(resources).forEach((name) => {
-      var type = resources[name].Type;
-      if (type === 'AWS::Lambda::Function') {
+      if (resources[name].Type === 'AWS::Lambda::Function') {
         resources[name].Properties.TracingConfig = {
           Mode: 'Active',
         };
